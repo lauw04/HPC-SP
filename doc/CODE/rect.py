162e7932a2d32e4008bf782b7f3352eeee089554
J.Antoine <justineantoine17@gmail.com>
2019-04-04 09:11:56 +0200

    Il y avait des soucis

---
+++ doc/CODE/rect.py
@@ -0,0 +1,33 @@
+#−∗−coding :  utf−8−∗−
+
+def rectangles(f,a,b,n):
+	"""Voici la fonction qui calcule l'intégrale d'une fonction donnée en paramètre par la méthode des rectangles
+  
+	:param function f: la fonction dont on calcule l'intégrale par la méthode des rectangles
+	:param float a: le début de l'intervalle
+	:param float b: la fin de l'intervalle
+	:param int n: le nombre de pas (i.e. d'intervalles) 
+	:returns: la valeur de l'intégrale
+	:rtype: float
+	"""
+	S=0
+	for i in xrange (0 ,n):
+		xi=a+(b-a)*i/float(n)
+		xj=a+(b-a)*(i+1)/float(n)
+		S+= f((xi+xj)/2.0)*(xj-xi)
+	return S
+
+def fn(x):
+	"""Voici la fonction qui définit une fonction mathématique et la calcule en un point donné en argument
+
+	:param float x: le point auquel on calcule la fonction 
+	:returns: la valeur de la fonction au point x
+	:rtype: float
+	"""
+	return 4.0/(1+(x-3)*(x-3))
+	
+def main():
+	"""Exécute la fonction
+	"""
+	print(rectangles(fn,0.,5.,100))
+	
---
+++ doc/source/modules.rst
@@ -0,0 +1,8 @@
+Programme qui calcule l'intégrale d'une fonction
+================================================
+.. image:: ../image.png
+
+.. toctree::
+   :maxdepth: 4
+	
+   tp
